a388cd9525885532c3143f0cee5f707a31002d5b
anushakadava <93367686+anushakadava@users.noreply.github.com>
2021-12-07 14:47:39 +0530

    Add files via upload

---
+++ perfect.r.not.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	intnum,num1,sum,i;
+	printf("enter a number");
+	scanf("%",&num);
+	num=num;
+	while(i=1,i<n,i++)
+	{
+	n=i+((1%n)==0)
+	sum=sum=i;	
+       }
+	if(num1==sum)
+	{
+	printf("it is a perfect number");
+	}
+	else
+	{
+		printf("it is not a perfect number");
+	}
+	
+}
